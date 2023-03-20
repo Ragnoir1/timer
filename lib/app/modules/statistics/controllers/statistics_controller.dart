942f74619a6b417171a9cf802c503bec29c7952e
EgorBrodsky <flatron0018@gmail.com>
2023-03-20 18:46:39 +0400

    Sticky

--- lib/app/modules/statistics/controllers/statistics_controller.dart
+++ lib/app/modules/statistics/controllers/statistics_controller.dart
@@ -3,13 +3,19 @@ import 'package:timer/app/data/data_job.dart';
 import 'package:timer/app/modules/categories/controllers/categories_controller.dart';
 
 class StatisticsController extends GetxController {
-  RxList<DataJob> ollLists = RxList<DataJob>([
-    CategoriesController.to.listJob,
-    CategoriesController.to.listSport,
-    CategoriesController.to.listHobby,
-    CategoriesController.to.listEducation,
-  ].expand((e) => e).toList());
-
+  List<DataJobClass> get ollLists => [
+      DataJobClass  (
+        name: "rabota",
+        list:CategoriesController.to.listJob),
+        DataJobClass  (
+        name: "listSport",
+        list:CategoriesController.to.listSport),DataJobClass  (
+        name: "listHobby",
+        list:CategoriesController.to.listHobby),DataJobClass  (
+        name: "listEducation",
+        list:CategoriesController.to.listEducation),
+        
+      ];
   final count = 0.obs;
   @override
   void onInit() {
@@ -24,4 +30,10 @@ class StatisticsController extends GetxController {
   @override
   void onClose() {}
   void increment() => count.value++;
+}
+
+class DataJobClass {
+  String name;
+  List<DataJob> list;
+  DataJobClass({required this.list , required this.name});
 }
--- lib/app/modules/statistics/views/statistics_view.dart
+++ lib/app/modules/statistics/views/statistics_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:sticky_headers/sticky_headers.dart';
 import 'package:timer/app/widgets/space_around.dart';
 import 'package:timer/app/widgets/statistics_banner.dart';
 
@@ -22,19 +22,42 @@ class StatisticsView extends GetView<StatisticsController> {
           Obx(
             () => Container(
               height: 450,
-              child: ListView.separated(
-                itemCount: controller.ollLists.length,
-                separatorBuilder: (BuildContext context, int index) {
-                  return SizedBox(
-                    height: 10,
-                  );
-                },
-                itemBuilder: ((context, index) => StatisticsBanner(
-                      text: controller.ollLists[index].label,
-                      time:
-                          "${(controller.ollLists[index].time ~/ 60).toString().padLeft(2, "0")}:${(controller.ollLists[index].time % 60).toString().padLeft(2, "0")}",
-                    ).paddingSymmetric(horizontal: 16)),
-              ),
+              child: ListView(children: [
+                ...controller.ollLists
+                    .map((e) => e.list.isNotEmpty
+                        ? StickyHeader(
+                            header: Container(
+                              height: 20,
+                              width: Get.width,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(color: Colors.red),
+                              child: Text(e.name),
+                            ),
+                            content: Column(
+                              children: [
+                                ...e.list
+                                    .map((job) => StatisticsBanner(
+                                          text: job.label,
+                                          time:
+                                              "${(job.time ~/ 60).toString().padLeft(2, "0")}:${(job.time % 60).toString().padLeft(2, "0")}",
+                                        ).paddingSymmetric(
+                                            horizontal: 16, vertical: 10))
+                                    .toList()
+                              ],
+                            ))
+                        : SizedBox())
+                    .toList(),
+              ]
+                  // itemCount: controller.ollLists.length,
+                  // separatorBuilder: (BuildContext context, int index) {
+                  //   return SizedBox(
+                  //     height: 10,
+                  //   );
+                  // },
+                  // itemBuilder:
+                  // ((context, index) =>
+
+                  ),
             ),
           )
         ],
